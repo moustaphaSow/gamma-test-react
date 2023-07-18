762f62d47bed7a9d8e5ea1c9d2434867a05196a8
moustaphaSow <moustapha262@gmail.com>
2023-07-18 12:57:51 +0200

    TDD and Last version

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import FizzBuzz from './FizzBuzz';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('FizzBuzz Component', () => {
+  test('renders FizzBuzz sequence from 1 to 100', () => {
+    render(<FizzBuzz />);
+    
+    for (let i = 1; i <= 100; i++) {
+      const fizzBuzzOutput = screen.getByText(String(i));
+      expect(fizzBuzzOutput).toBeInTheDocument();
+    }
+  });
+
+  test('renders Fizz for numbers divisible by 3 or contain digit 3', () => {
+    render(<FizzBuzz />);
+    
+    const fizzNumbers = [3, 6, 9, 12, 13, 18, 21, 23, 27, 30];
+    
+    fizzNumbers.forEach((num) => {
+      const fizzOutput = screen.getByText('Fizz');
+      expect(fizzOutput).toBeInTheDocument();
+    });
+  });
+
+  test('renders Buzz for numbers divisible by 5 or contain digit 5', () => {
+    render(<FizzBuzz />);
+    
+    const buzzNumbers = [5, 10, 15, 25, 35, 40, 52, 55, 58, 59];
+    
+    buzzNumbers.forEach((num) => {
+      const buzzOutput = screen.getByText('Buzz');
+      expect(buzzOutput).toBeInTheDocument();
+    });
+  });
+
+  test('renders FizzBuzz for numbers divisible by 3 and 5', () => {
+    render(<FizzBuzz />);
+    
+    const fizzBuzzNumbers = [15, 30, 45, 60, 75, 90, 93, 95, 99];
+    
+    fizzBuzzNumbers.forEach((num) => {
+      const fizzBuzzOutput = screen.getByText('FizzBuzz');
+      expect(fizzBuzzOutput).toBeInTheDocument();
+    });
+  });
 });
